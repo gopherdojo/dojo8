4f0f1ec7dfad81da458aa07e793ef96c58ea39ef
ryo_ishibashi <ryokky59@gmail.com>
2020-07-10 22:54:42 +0900

    他の方のレビュー見ながらリファクタリング

--- kadai1/ryokky59/convert/convert.go
+++ kadai1/ryokky59/convert/convert.go
@@ -1,96 +1,54 @@
-
 package convert
 
 import (
-	"flag"
 	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"os"
 	"path/filepath"
 	"strings"
-	"image"
-	"image/png"
-	"image/jpeg"
-)
 
-var (
-	dir  string
-	from string
-	to   string
+	"golang.org/x/image/bmp"
+	"golang.org/x/image/tiff"
 )
-
-func init() {
-	flag.StringVar(&dir, "dir", "./", "対象のディレクトリ")
-	flag.StringVar(&from, "from", "jpg", "変換する対象の画像の拡張子")
-	flag.StringVar(&to, "to", "png", "変換した後の画像の拡張子")
-	flag.Parse()
-}
 
 // Exec 画像の変換を実行する
-func Exec() error {
-	converter, err := newImgConverter(dir, from, to)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	err = filepath.Walk(converter.dir, func(path string, info os.FileInfo, err error) error {
-		if strings.HasSuffix(info.Name(), from) {
-			converter.name = strings.TrimRight(info.Name(), from)
+func Exec(converter *ImgConverter) error {
+	err := filepath.Walk(converter.Dir, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(info.Name(), converter.From) {
+			converter.Name = strings.TrimRight(info.Name(), converter.From)
 			err := converter.convert()
 			if err != nil {
-				fmt.Println(err)
-				return nil
+				return err
 			}
 		}
 		return nil
 	})
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return err
 	}
 
 	return nil
 }
 
-type imgConverter struct {
-	dir  string
-	name string
-	from string
-	to   string
+// ImgConverter 画像の拡張子を変換する
+type ImgConverter struct {
+	Dir  string
+	Name string
+	From string
+	To   string
 }
 
-func newImgConverter(dir, from, to string) (*imgConverter, error) {
-	if from != "png" && from != "jpeg" && from != "jpg" {
-		return nil, fmt.Errorf("Unsupported extension in from")
-	}
-
-	if to != "png" && to != "jpeg" && to != "jpg" {
-		return nil, fmt.Errorf("Unsupported extension in to")
-	}
-
-	if from == to {
-		return nil, fmt.Errorf("from and to are conflict")
-	}
-
-	if f, err := os.Stat(dir); os.IsNotExist(err) || !f.IsDir() {
-		return nil, fmt.Errorf("Directory does not exist")
-	}
-
-	return &imgConverter{
-		dir: dir,
-		from: from,
-		to: to,
-	}, nil
-}
-
-func (c *imgConverter) convert() error {
+func (c *ImgConverter) convert() error {
 	file, err := os.Open(c.path())
-	defer file.Close()
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
-	img, _, err :=  image.Decode(file)
+	img, _, err := image.Decode(file)
 	if err != nil {
 		return err
 	}
@@ -101,7 +59,7 @@ func (c *imgConverter) convert() error {
 		return err
 	}
 
-	switch c.to {
+	switch strings.ToLower(c.To) {
 	case "png":
 		err = png.Encode(dst, img)
 		if err != nil {
@@ -112,15 +70,32 @@ func (c *imgConverter) convert() error {
 		if err != nil {
 			return err
 		}
+	case "gif":
+		err = gif.Encode(dst, img, nil)
+		if err != nil {
+			return err
+		}
+	case "bmp":
+		err = bmp.Encode(dst, img)
+		if err != nil {
+			return err
+		}
+	case "tiff":
+		err = tiff.Encode(dst, img, nil)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Unsupported extension")
 	}
 
 	return nil
 }
 
-func (c *imgConverter) path() string {
-	return c.dir + c.name + c.from
+func (c *ImgConverter) path() string {
+	return c.Dir + c.Name + c.From
 }
 
-func (c *imgConverter) dstPath() string {
-	return c.dir + c.name + c.to
+func (c *ImgConverter) dstPath() string {
+	return c.Dir + c.Name + c.To
 }
--- kadai1/ryokky59/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/gopherdojo/dojo8/kadai1/ryokky59/convert"
-
-func main() {
-	convert.Exec()
-}
